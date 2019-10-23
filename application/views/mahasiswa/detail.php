97b083200a9c16a9b9130c3a8afe42f57497c01e
Habibullah <habibullahsyech@yandex.ru>
2019-10-23 22:34:53 +0700

    Melanjutkan kodingan PHPMVC dari video ngobar ke 11

--- application/views/mahasiswa/detail.php
+++ application/views/mahasiswa/detail.php
@@ -1,11 +1,19 @@
-<div class="container mt-5">
-    <div class="card" style="width: 18rem;">
-        <div class="card-body">
-            <h5 class="card-title"><?= $data['mhs']['nama']; ?></h5>
-            <h6 class="card-subtitle mb-2 text-muted"><?= $data['mhs']['nrp']; ?></h6>
-            <p class="card-text"><?= $data['mhs']['email']; ?></p>
-            <p class="card-text"><?= $data['mhs']['jurusan']; ?></p>
-            <a href="<?= BASEURL; ?>mahasiswa" class="card-link">Kembali</a>
+<div class="container">
+    <div class="row mt-3">
+        <div class=çol-md-6>
+
+            <div class="card">
+                <div class="card-header">
+                Detail Data Mahasiswa
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title"><?= $mahasiswa['nama']; ?></h5>
+                    <h6 class="card-subtitle mb-2 text-muted"><?= $mahasiswa['nrp']; ?></h6>
+                    <p class="card-text"><?= $mahasiswa['email']; ?></p>
+                    <p class="card-text"><?= $mahasiswa['jurusan']; ?></p>
+                    <a href="<?= base_url(); ?>mahasiswa" class="btn btn-primary">Kembali</a>
+                </div>
+            </div>
         </div>
     </div>
 </div>
--- application/views/mahasiswa/tambah.php
+++ application/views/mahasiswa/tambah.php
@@ -16,14 +16,16 @@
                         <div class="form-group">
                             <label for="nama">Nama</label>
                             <input type="text" name="nama" class="form-control" id="nama">
+                            <small class="form-text text-danger"><?= form_error('nama'); ?></small>
                         </div>
                         <div class="form-group">
                             <label for="nrp">NRP</label>
                             <input type="text" name="nrp" class="form-control" id="nrp">
-                        </div>
+                            <small class="form-text text-danger"><?= form_error('nrp'); ?></small>                        </div>
                         <div class="form-group">
                             <label for="email">Email</label>
                             <input type="text" name="email" class="form-control" id="email">
+                            <small class="form-text text-danger"><?= form_error('email'); ?></small>
                         </div>
                         <div class="form-group">
                             <label for="jurusan">Jurusan</label>
